71d816f57278b45c32a4babc88b8e74efa768013
BaturaDmitry <koonnot@mail.ru>
2021-12-15 08:14:31 +0300

    added test class

---
+++ TestProject1/TestClass.cs
@@ -0,0 +1,83 @@
+﻿namespace TestProject1
+{
+    interface ISomeInterface
+            {
+                void Firstmethod();
+
+                void Secondmethod();
+            }
+        
+            class Class : ISomeInterface
+            {
+        
+                public void Firstmethod()
+                {
+                    throw new System.NotImplementedException();
+                }
+        
+                public void Secondmethod()
+                {
+                    throw new System.NotImplementedException();
+                }
+            }
+        
+            interface ITestClass
+            {
+                void Firstmth();
+        
+                void Secondmth();
+            }
+        
+            class TestClass : ITestClass
+            {
+                public ISomeInterface isomeInterface;
+        
+                public TestClass(ISomeInterface isomeInterface)
+                {
+                    this.isomeInterface = isomeInterface;
+                }
+        
+                public void Firstmth()
+                {
+                    throw new System.NotImplementedException();
+                }
+        
+                public void Secondmth()
+                {
+                    throw new System.NotImplementedException();
+                }
+            }
+        
+            class TestClass2 : ITestClass
+            {
+                public ISomeInterface isomeInterface;
+        
+                public TestClass2(ISomeInterface isomeInterface)
+                {
+                    this.isomeInterface = isomeInterface;
+                }
+        
+                public void Firstmth()
+                {
+                    throw new System.NotImplementedException();
+                }
+        
+                public void Secondmth()
+                {
+                    throw new System.NotImplementedException();
+                }
+            }
+        
+            class Class2 : ISomeInterface
+            {
+                public void Firstmethod()
+                {
+                    throw new System.NotImplementedException();
+                }
+        
+                public void Secondmethod()
+                {
+                    throw new System.NotImplementedException();
+                }
+            }
+}
